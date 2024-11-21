1733de3b21124a7303e5941fb86b2da3563424b1
Bruno Coviello <bcovies@gmail.com>
2024-11-21 11:09:19 -0300

    Install Lazy and treesitter

---
+++ .config/nvim/lua/config/nvim-treesitter.lua
@@ -0,0 +1,31 @@
+local options = {
+  ensure_installed = {
+    "lua",
+    "luadoc",
+    "printf",
+    "vim",
+    "vimdoc",
+    "python",
+    "javascript",
+    "typescript",
+    "go",
+    "rust",
+    "c",
+    "cpp",
+    "java",
+    "html",
+    "css",
+    "bash",
+    "yaml",
+    "json",
+    "regex"
+  },
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+
+  indent = { enable = true },
+}
+
+return options

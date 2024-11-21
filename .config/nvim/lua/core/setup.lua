215d9d9699aacdd359b7eedce8f9b4631daa73bd
Bruno Coviello <bcovies@gmail.com>
2024-11-21 11:09:51 -0300

    fix lazy

--- .config/nvim/lua/core/setup.lua
+++ .config/nvim/lua/core/setup.lua
@@ -3,7 +3,7 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     {
-      import = "plugins/nvim-treesitter",
+      import = "plugins",
     },
   },
   -- Configure any other settings here. See the documentation for more details.
